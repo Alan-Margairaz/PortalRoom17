e7e29e02805e7912d2e82d56a4bd77ac9f691129
Alan Margairaz <apmargairaz@gmail.com>
2023-02-03 10:05:23 +0100

    Designed and finished the EnergyPelletCatcher to destroy EnergyPellets on trigger thanks to a tag

--- Portal_Room17/Assets/Scripts/EnergyPellet.cs
+++ Portal_Room17/Assets/Scripts/EnergyPellet.cs
@@ -6,6 +6,7 @@ public class EnergyPellet : MonoBehaviour
 {
     int reboundCount = 0;
 
+    // Grâce au Physics Material, on peut contrôler le nombre de rebonds voulu des boules d'énergie:
     private void OnCollisionEnter(Collision collision)
     {
         if (reboundCount <= 6)
@@ -13,6 +14,15 @@ public class EnergyPellet : MonoBehaviour
             reboundCount++;
         }
         else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Les triggers placés sur les 2 éléments de l'EnergyPelletCatcher permettent de détruire les boules d'énergie: 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PelletCatcher")
         {
             Destroy(gameObject);
         }
