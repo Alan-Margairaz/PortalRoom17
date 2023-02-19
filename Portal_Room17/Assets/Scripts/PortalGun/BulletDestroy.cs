f336c8da076ff442d038b0b25087c8dec26e1d0a
Alan Margairaz <apmargairaz@gmail.com>
2023-02-19 17:40:02 +0100

    Useless attempt
    Tried to add portals, in vain

--- Portal_Room17/Assets/Scripts/PortalGun/BulletDestroy.cs
+++ Portal_Room17/Assets/Scripts/PortalGun/BulletDestroy.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class BulletDestroy : MonoBehaviour
 {
+    public GameObject portalPrefab;
+
     private void OnCollisionEnter(Collision collision)
     {
+        Instantiate(portalPrefab, collision.transform.position, Quaternion.LookRotation(Vector3.forward));
         Destroy(gameObject);
     }
 }
