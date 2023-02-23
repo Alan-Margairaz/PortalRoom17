de5c18e3f18e54aeb097781b65abb964690c2e08
Alan Margairaz <apmargairaz@gmail.com>
2023-02-23 16:57:38 +0100

    Portal(2)
    Fixed bugs linked to portals, added teleportation.
    Working on a way to limitate the max number of portals you place.

--- Portal_Room17/Assets/Scripts/Portals/PortalScript.cs
+++ Portal_Room17/Assets/Scripts/Portals/PortalScript.cs
@@ -4,57 +4,48 @@ using UnityEngine;
 
 public class PortalScript : MonoBehaviour
 {
-    // Variables de TP du joueur/cube:
+    // Variables de téléportation:
     bool hasPassedPortal = false;
     private CharacterController characterController;
 
     // Variables de référence des portails:
     public GameObject bluePortal, orangePortal;
-    Vector3 orangePortalPos, bluePortalPos;
-    
+
 
     private void Start()
     {
+        // Récupération du character controller du joueur pour l'activation/désactivation au moment de la téléportation:
         characterController = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
-    }
-
-    private void Update()
-    {
-        // Calcul de la position du portail orange:
-        orangePortalPos = orangePortal.transform.position +
-                          orangePortal.transform.right * 2;
-
-        // Calcul de la position du portail bleu:
-        bluePortalPos = bluePortal.transform.position +
-                        bluePortal.transform.right * 2;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("triggered TP");
         // Check des objets qui peuvent passer le portail (le joueur, le cube ou les boules d'énergie):
         if(other.CompareTag("Player") || other.CompareTag("CompanionCube") || other.CompareTag("EnergyPellet"))
         {
+            // Désactivation du character controller car il empêche la téléportation:
+            characterController.enabled = false;
             if (hasPassedPortal == false)
             {
-                // Désactivation du character controller car il empêche la téléportation:
-                characterController.enabled = false;
-
+                // Cas de si le joueur entre en trigger avec un portail bleu:
                 if (gameObject.Equals(bluePortal))
                 {
-                    // Téléportation du joueur à la position (un peu devant) du 2nd portail:
-                    other.gameObject.transform.position = orangePortalPos;
+                    // Destination du joueur à travers le portail bleu:
+                    Vector3 blueDestinationPos = GameObject.FindGameObjectWithTag("OrangePortal").transform.position + (2 * GameObject.FindGameObjectWithTag("OrangePortal").transform.forward);
 
-                    // Reset du booléen pour savoir si oui ou non la téléportation a été faite & réactivation du character controller:
+                    // Téléportation du joueur à la position du 2nd portail:
+                    other.gameObject.transform.position = blueDestinationPos;
+
+                    // Reset du booléen pour avoir un suivi de si oui ou non la téléportation a été faite & réactivation du character controller à la sortie du portail:
                     hasPassedPortal = true;
                     characterController.enabled = true;
                 }
-
+                // Même processus dans le cas du portail orange:
                 if (gameObject.Equals(orangePortal))
                 {
-                    // Téléportation du joueur à la position (un peu devant) du 2nd portail:
-                    other.gameObject.transform.position = bluePortalPos;
-
-                    // Reset du booléen pour savoir si oui ou non la téléportation a été faite & réactivation du character controller:
+                    Vector3 orangeDestinationPos = GameObject.FindGameObjectWithTag("BluePortal").transform.position + (2 * GameObject.FindGameObjectWithTag("BluePortal").transform.forward);
+                    other.gameObject.transform.position = orangeDestinationPos;
                     hasPassedPortal = true;
                     characterController.enabled = true;
                 }
@@ -63,9 +54,9 @@ public class PortalScript : MonoBehaviour
             {
                 // Réactivation du character controller lors de la sortie du portail:
                 characterController.enabled = true;
-                Debug.Log(hasPassedPortal);
-                Debug.Log("Has NOT TP");
             }
         }
     }
-}
+} 
+
+
--- Portal_Room17/Assets/Scripts/Portals/PortalSpawn.cs
+++ Portal_Room17/Assets/Scripts/Portals/PortalSpawn.cs
@@ -1,63 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 
 public class PortalSpawn : MonoBehaviour
 {
-    // Variables de détection des murs où un portail peut spawn & du type de balle tiré:
-    public GameObject orangeBullet;
-    public GameObject blueBullet;
-    public LayerMask spawnableLayerMask;
-    private RaycastHit hit;
+    // Variable de la caméra pour la détection des surfaces où un portail peut spawn:
     GameObject playerCamera;
 
-    // Variables d'instantiation des portails:
+    // Variables d'instantiation des portails en fonction des balles tirées:
     public GameObject bluePortalPrefab;
     public GameObject orangePortalPrefab;
 
+    public GameObject orangeBullet;
+    public GameObject blueBullet;
+
+    // Variables pour n'avoir qu'un seul portail de chaque couleur sur la map:
+    private bool isPlaced = false;
+    private GameObject[] portalArray;
 
     private void Start()
     {
-        // Récupération de la caméra du joueur pour le Raycast:
+        //Récupération de la caméra du joueur pour le Raycast:
         playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
+    private void Update()
+    {
+        PortalCount();
+    }
+
+    // Apparition du portail lorsque la balle collide avec une surface permettant le spawn d'un portail:
     private void OnCollisionEnter(Collision collider)
     {
-        // Test de si le mur permet l'apparition d'un portail ou non grâce à un Raycast:
-        if (Physics.Raycast(
-            playerCamera.transform.position,
-            playerCamera.transform.forward,
-            out hit, spawnableLayerMask))
+        // Coordonnées X et Y par rapport à l'écran afin de mettre le Raycast au centre de l'écran:
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        // Création du Raycast en partant de la caméra du joueur et au centre de l'écran:
+        RaycastHit hit;
+        Ray ray = playerCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+        
+        // On check si le Raycast touche quelque chose:
+        if (Physics.Raycast(ray, out hit))
         {
-            if (collider.collider.CompareTag("Spawnable"))
+            // Calcul de la rotation de la surface sur laquelle va spawn le portail grâce à sa normale:
+            Quaternion hitObjectRotation = Quaternion.LookRotation(hit.normal);
+
+            // Test de si le mur permet l'apparition d'un portail ou non ainsi que du type de balle tiré:
+            if (collider.collider.CompareTag("Spawnable") && gameObject.Equals(blueBullet))
             {
-                // Calcul de la rotation de l'objet sur lequel un portail va apparaître grâce à la normale de l'objet 'obstacle':
-                Quaternion hitObjectRotation = Quaternion.LookRotation(hit.normal);
-                SpawnPortal(hitObjectRotation);
-            } else
+                // Instantiation du portail bleu au point ou la balle touche et avec la rotation de la surface touchée:
+                Instantiate(bluePortalPrefab, hit.point + new Vector3(0.1f,0,0), hitObjectRotation);
+                // Destruction de la balle après l'instantiation
+                Destroy(blueBullet);
+                isPlaced = true;
+            }
+            // Pareil pour le portail orange:
+            else if (collider.collider.CompareTag("Spawnable") && gameObject.Equals(orangeBullet))
             {
-                // Si le projectile collide avec autre chose ou le portail ne peut pas spawn, il est détruit:
+                Instantiate(orangePortalPrefab, hit.point + new Vector3(0.1f, 0, 0), hitObjectRotation);
+                Destroy(orangeBullet);
+                
+                isPlaced = true;
+            }
+            // Si le projectile collide avec une surface non spawnable, il est détruit:
+            else
+            {
                 Destroy(gameObject);
+                isPlaced = false;
             }
         }
     }
 
-    private void SpawnPortal(Quaternion hitObjectRotation)
+    private void PortalCount()
     {
-        // On fait apparaître un portail bleu ou orange en fonction du projectile tiré:
-        if (gameObject.Equals(blueBullet))
+        if (isPlaced == true)
         {
-            // Instantiation du portail bleu & destruction du projectile:
-            Instantiate(bluePortalPrefab, gameObject.transform.position, hitObjectRotation);
-            Destroy(gameObject);
-        }
+            for (int i = 0; i < 2; i++)
+            {
 
-        if (gameObject.Equals(orangeBullet))
-        {
-            // Instantiation du portail orange & destruction du projectile:
-            Instantiate(orangePortalPrefab, gameObject.transform.position, hitObjectRotation);
-            Destroy(gameObject);
+            }
         }
     }
 }
